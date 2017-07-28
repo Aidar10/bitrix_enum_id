b8fa4d428471a2ce8bbd4fc82d59f8926aaba825
Aidar10 <studdar@yandex.ru>
2017-07-28 13:55:01 +0300

    Create news-detail.php

---
+++ news-detail.php
@@ -0,0 +1,11 @@
+<? if ($arResult['PROPERTIES']['INTER']) { ?>
+    <? foreach ($arResult['PROPERTIES']['INTER']['VALUE_ENUM_ID'] as $arItem) { ?>
+        <? if ($arItem == 1) { ?>
+            <div class="col-md-6">
+                <img src="/local/static/images/two_beds.png" alt=""/>
+                <span>Двуспальная кровать</span>
+            </div>
+        <? } ?>
+    <? } ?>
+<? } ?>
+</section>
